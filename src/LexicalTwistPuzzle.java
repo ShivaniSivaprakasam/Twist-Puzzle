fbdc6b6752cd210ca3291e23bdf9b3174ca417e2
shivani <shivaniavanikkarasi@gmail.com>
2026-05-09 18:03:38 +0530

    [Shivani] : UC1 - added : application title display for lexical twist puzzle

---
+++ src/LexicalTwistPuzzle.java
@@ -0,0 +1,23 @@
+/**
+ * ------------------------------------------------------------
+ * Lexical Twist Puzzle
+ * ------------------------------------------------------------
+ * UC1 - Display Puzzle Title
+ *
+ * This program displays the application title.
+ *
+ * Author : Shivani
+ * ------------------------------------------------------------
+ */
+
+public class LexicalTwistPuzzle {
+
+    /**
+     * Main method - Entry point of the application
+     */
+    public static void main(String[] args) {
+
+        // Display application title
+        System.out.println("Lexical Twist Puzzle");
+    }
+}
